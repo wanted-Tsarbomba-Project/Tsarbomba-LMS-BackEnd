781123979964921c2aabb3bb65844f23275e418c
Dong_gu <dkdlwkr207@gmail.com>
2026-05-20 00:34:53 +0900

    feat. 로그아웃/토큰 재발급 API + 메서드 레벨 보안 도입

--- src/main/java/com/wanted/codebombalms/global/config/SecurityConfig.java
+++ src/main/java/com/wanted/codebombalms/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.wanted.codebombalms.global.security.exception.CustomAuthenticationEnt
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
