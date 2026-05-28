52cf80016664850ceb63dfa901171ae4f02dcf2e
Dong_gu <dkdlwkr207@gmail.com>
2026-05-28 14:36:09 +0900

    feat: 로그인 응답에 role 추가

--- src/main/java/com/wanted/codebombalms/auth/presentation/api/dto/response/LoginResponse.java
+++ src/main/java/com/wanted/codebombalms/auth/presentation/api/dto/response/LoginResponse.java
@@ -2,9 +2,9 @@ package com.wanted.codebombalms.auth.presentation.api.dto.response;
 
 import com.wanted.codebombalms.auth.application.dto.LoginResult;
 
-public record LoginResponse(String nickname) {
+public record LoginResponse(String nickname, String role) {
 
     public static LoginResponse from(LoginResult result) {
-        return new LoginResponse(result.nickname());
+        return new LoginResponse(result.nickname(), result.role(). name());
     }
 }
