736c1b3a7c572cef42832d96e6c20f0a6de7fbb6
rladydwns2361 <gimyongjun796@gmail.com>
2026-06-22 18:09:05 +0900

    cl 문제 해결 제발!

--- src/test/java/com/wanted/codebombalms/CodeBombaLmsApplicationTests.java
+++ src/test/java/com/wanted/codebombalms/CodeBombaLmsApplicationTests.java
@@ -3,7 +3,9 @@ package com.wanted.codebombalms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "jwt.secret=MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE="
+})
 class CodeBombaLmsApplicationTests {
 
     @Test
