f3a34576e8abb96ca99666874a74a32ab28ed906
dongdong <dkdlwkr207@gmail.com>
2026-06-30 18:19:54 +0900

    fix: 로그인 기록 시간대 수정(미국 -> 한국)

--- src/main/java/com/wanted/codebombalms/global/infrastructure/config/JpaAuditingConfig.java
+++ src/main/java/com/wanted/codebombalms/global/infrastructure/config/JpaAuditingConfig.java
@@ -1,9 +1,20 @@
 package com.wanted.codebombalms.global.infrastructure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 public class JpaAuditingConfig {
+
+    @Bean
+    public DateTimeProvider auditingDateTimeProvider(Clock clock) {
+        return () -> Optional.of(LocalDateTime.now(clock));
+    }
 }
