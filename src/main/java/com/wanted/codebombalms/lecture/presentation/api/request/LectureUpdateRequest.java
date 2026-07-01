751effdba5a88fcb1c2a09b12226f6b4ccdbbf80
RabbitHunter <lgw1234562@gmail.com>
2026-07-01 16:10:29 +0900

    Update LectureUpdateRequest.java

--- src/main/java/com/wanted/codebombalms/lecture/presentation/api/request/LectureUpdateRequest.java
+++ src/main/java/com/wanted/codebombalms/lecture/presentation/api/request/LectureUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.wanted.codebombalms.lecture.presentation.api.request;
 
 import com.wanted.codebombalms.lecture.domain.model.LectureStatus;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -16,6 +17,7 @@ public record LectureUpdateRequest(
         @Size(max = 500, message = "썸네일 URL은 500자 이하로 입력해야 합니다.")
         String thumbnailUrl,
 
+        @NotNull(message = "문제 카테고리 ID는 필수입니다.")
         @Positive(message = "문제 카테고리 ID는 양수여야 합니다.")
         Long problemCategoryId,
 
